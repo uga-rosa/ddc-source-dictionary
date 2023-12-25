a6724628137e689966fcdf608297eacfc0e13efc
uga-rosa <uga6603@gmail.com>
2023-12-25 21:33:21 +0900

    Add param databasePath

--- denops/@ddc-sources/dictionary.ts
+++ denops/@ddc-sources/dictionary.ts
@@ -19,12 +19,23 @@ function decapitalize(str: string): string {
   return str.charAt(0).toLowerCase() + str.slice(1);
 }
 
-type Cache = {
-  path: string;
-  mtime: number;
-  trie: Trie;
-  active: boolean;
-};
+function same(x: unknown, y: unknown): boolean {
+  return JSON.stringify(x) === JSON.stringify(y);
+}
+
+type Cache =
+  & {
+    path: string;
+    mtime: number;
+    active: boolean;
+  }
+  & ({
+    trie: Trie;
+    db?: undefined;
+  } | {
+    trie?: undefined;
+    db: Deno.Kv;
+  });
 
 type Params = {
   paths: string[];
@@ -32,28 +43,43 @@ type Params = {
   firstCaseInsensitive: boolean;
   showPath: boolean;
   documentCommand: string[];
+  databasePath: string;
 };
 
 export class Source extends BaseSource<Params> {
   #dictCache: Record<string, Cache> = {};
-  #prePaths = "";
+  #prevPaths: string[] = [];
+
+  #db?: Deno.Kv;
+  async maybeDB(path: string): Promise<Deno.Kv | undefined> {
+    try {
+      if (this.#db == null && path) {
+        this.#db = await Deno.openKv(path);
+      }
+      return this.#db;
+    } catch {
+      // failed to open database
+    }
+  }
 
   events = ["Initialize", "InsertEnter"];
   async onEvent({ sourceParams }: OnEventArguments<Params>): Promise<void> {
-    if (JSON.stringify(sourceParams.paths) !== this.#prePaths) {
-      await this.update(sourceParams.paths);
+    if (!same(sourceParams.paths, this.#prevPaths)) {
+      await this.update(
+        sourceParams.paths,
+        await this.maybeDB(sourceParams.databasePath),
+      );
     }
   }
 
   #lock = new Lock(this.#dictCache);
   #onGoing = false;
-  async update(paths: string[]): Promise<void> {
+  async update(paths: string[], db?: Deno.Kv): Promise<void> {
     if (this.#onGoing) {
       return;
     }
     this.#onGoing = true;
-
-    this.#prePaths = JSON.stringify(paths);
+    this.#prevPaths = paths;
 
     // Deactivate old caches.
     for (const cache of Object.values(this.#dictCache)) {
@@ -67,46 +93,100 @@ export class Source extends BaseSource<Params> {
       const mtime = stat.mtime?.getTime();
       // If there is no update, the previous cache is used as is.
       if (mtime != null && this.#dictCache[path]?.mtime === mtime) {
-        this.#dictCache[path].active = true;
+        await this.#lock.lock((dictCache) => {
+          dictCache[path].active = true;
+        });
         return;
       }
 
-      const trie = new Trie();
+      if (
+        db != null && mtime != null &&
+        (await db.get([path, "mtime"])).value === mtime
+      ) {
+        await this.#lock.lock((dictCache) => {
+          dictCache[path] = {
+            path,
+            mtime: mtime ?? -1,
+            active: true,
+            db,
+          };
+        });
+        return;
+      }
+
       const file = await Deno.open(path);
       const lineStream = file.readable
         .pipeThrough(new TextDecoderStream())
         .pipeThrough(new TextLineStream());
-      for await (const line of lineStream) {
-        line.split(/\s+/).forEach((word) => {
-          if (word !== "") {
-            trie.insert(word);
+      if (db != null) {
+        let atm = db.atomic();
+        let count = 0;
+        for await (const line of lineStream) {
+          for (const word of line.split(/\s+/)) {
+            if (word !== "") {
+              atm = atm.set([...word], word);
+              if (++count >= 1000) {
+                await atm.commit();
+                atm = db.atomic();
+                count = 0;
+              }
+            }
           }
+        }
+        await atm.commit();
+        await db.set([path, "mtime"], mtime);
+        await this.#lock.lock((dictCache) => {
+          dictCache[path] = {
+            path,
+            mtime: mtime ?? -1,
+            active: true,
+            db,
+          };
+        });
+      } else {
+        const trie = new Trie();
+        for await (const line of lineStream) {
+          for (const word of line.split(/\s+/)) {
+            if (word !== "") {
+              trie.insert(word);
+            }
+          }
+        }
+        await this.#lock.lock((dictCache) => {
+          dictCache[path] = {
+            path,
+            mtime: mtime ?? -1,
+            active: true,
+            trie,
+          };
         });
       }
-
-      await this.#lock.lock((dictCache) => {
-        dictCache[path] = {
-          path,
-          mtime: mtime ?? -1,
-          trie,
-          active: true,
-        };
-      });
     }));
 
     this.#onGoing = false;
   }
 
-  search(prefix: string, showPath: boolean): Item[] {
-    return Object.values(this.#dictCache)
-      .filter((cache) => cache.active)
-      .flatMap((cache) => {
-        const info = showPath ? cache.path : "";
-        return cache.trie.search(prefix).map((word) => ({ word, info }));
-      });
+  async search(prefix: string, showPath: boolean): Promise<Item[]> {
+    const items: Item[] = [];
+    for (const cache of Object.values(this.#dictCache)) {
+      if (!cache.active) {
+        continue;
+      }
+      const info = showPath ? cache.path : "";
+      if (cache.trie) {
+        cache.trie.search(prefix).forEach((word) => items.push({ word, info }));
+      } else {
+        for await (
+          const entry of cache.db.list<string>({ prefix: [...prefix] })
+        ) {
+          items.push({ word: entry.value, info });
+        }
+      }
+    }
+    return items;
   }
 
-  gather({
+  async gather({
     sourceParams: params,
     completeStr,
   }: GatherArguments<Params>): Promise<DdcGatherItems> {
@@ -116,22 +196,22 @@ export class Source extends BaseSource<Params> {
       const isCapital = prefix.charAt(0) === prefix.charAt(0).toUpperCase();
       if (isCapital) {
         items = [
-          ...this.search(prefix, params.showPath),
-          ...this.search(decapitalize(prefix), params.showPath)
+          ...await this.search(prefix, params.showPath),
+          ...(await this.search(decapitalize(prefix), params.showPath))
             .map((item) => ({ ...item, word: capitalize(item.word) })),
         ];
       } else {
         items = [
-          ...this.search(prefix, params.showPath),
-          ...this.search(capitalize(prefix), params.showPath)
+          ...await this.search(prefix, params.showPath),
+          ...(await this.search(capitalize(prefix), params.showPath))
             .map((item) => ({ ...item, word: decapitalize(item.word) })),
         ];
       }
     } else {
-      items = this.search(prefix, params.showPath);
+      items = await this.search(prefix, params.showPath);
     }
     const isIncomplete = completeStr.length < params.exactLength;
-    return Promise.resolve({ items, isIncomplete });
+    return { items, isIncomplete };
   }
 
   #decoder = new TextDecoder();
@@ -180,6 +260,7 @@ export class Source extends BaseSource<Params> {
       firstCaseInsensitive: false,
       showPath: false,
       documentCommand: [],
+      databasePath: "",
     };
   }
 }
